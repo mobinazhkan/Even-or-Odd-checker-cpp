8d5815cefea734a4194ca7b152898b35e1541f10
Mobin Azhkan <86418845+mobinazhkan@users.noreply.github.com>
2025-12-27 18:47:25 +0330

    Add files via upload

---
+++ Zoj ya fard.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main();{
+    number a;
+    cout << "Enter your number:  ";
+    cin >> a;
+
+if(a%2==0)
+cout << "adade shoma zoj ast";
+
+else
+cout << "adade shoma fard ast"
+
+}
+return 0
